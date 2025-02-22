93a7973141573c0cc13d1ef96cd5cf585c041562
rangasanjuau <sanjay.ranga.au@gmail.com>
2025-02-22 14:23:14 +1100

    Enable springboot security

--- src/main/java/com/infyniteloop/isec/controller/MainController.java
+++ src/main/java/com/infyniteloop/isec/controller/MainController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MainController {
 
-    @GetMapping("/login")
-    public String login() {
+    @GetMapping("/hello")
+    public String hello() {
         return "Welcome to ISEC";
     }
 }
